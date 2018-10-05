af13c371d2be7942e15e84fa631d20b33ccc6a30
MiddleTime <nadoroge@gmail.com>
2018-10-05 14:49:07 +0300

    Testing

---
+++ src/FactoryMethodWithArray/CleaningCompany.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryMethodWithArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author inbox_bv8awe9
+ */
+public class CleaningCompany extends Company{
+        
+    @Override
+    List getEmploees() {
+        System.out.println("");
+        System.out.println("Starting company activity " +CleaningCompany.class.getSimpleName() +"\n");
+        List employee = new ArrayList(); //пример c коллекцией
+        employee.add(new Cleaner());
+        employee.add(new Cleaner());
+        employee.add(new Manager());
+        employee.add(new Cleaner());
+        employee.add(new Cleaner());
+        employee.add(new Cleaner());
+        employee.add(new Cleaner());
+        employee.add(new Manager());
+        
+        return employee; 
+    }
+    
+   
+   
+    
+}
---
+++ src/FactoryMethodWithArray/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryMethodWithArray;
+
+import java.util.List;
+
+
+/**
+ *
+ * @author inbox_bv8awe9
+ */
+public class Main {
+    public static void main(String[] args) {
+        Company gameCompamy = new GameDevCompany();
+        List computerWorkers= gameCompamy.getEmploees();
+        gameCompamy.startWorking(computerWorkers);
+        
+        Company cleaningCompany = new CleaningCompany();
+        List cleanWorkers= cleaningCompany.getEmploees();
+        gameCompamy.startWorking(cleanWorkers);
+    }
+}
