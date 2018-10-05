87c3d9bafb9deb17b9000008fd1bda046fe3766a
MiddleTime <nadoroge@gmail.com>
2018-10-05 13:10:27 +0300

    Testing

---
+++ src/Before/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Before;
+
+/**
+ *
+ * @author inbox_bv8awe9
+ */
+public class Main {
+    public static void main(String[] args) {
+        Company comp = new Company();
+        comp.createSoftware();
+//        new Company().createSoftware();  когда такое нужно?
+    }
+}
+
+ class Company{
+    void createSoftware(){
+        System.out.println("Starting company activity");
+        Designer desinger = new Designer(); 
+        desinger.designArchitecture();
+        Programmer programmer = new Programmer();
+        programmer.programmingCode();
+        Tester tester = new Tester();
+        tester.TestingCode();
+    }
+}
+
+class Designer {
+    void designArchitecture(){
+        System.out.println("I disinger. Create architecture!");
+    }
+}
+
+ class Programmer {
+    void programmingCode(){
+        System.out.println("I programmer. Programming code!");
+    }
+}
+
+class Tester {
+    void TestingCode(){
+        System.out.println("I tester. Testing code!");
+    }
+}
+
